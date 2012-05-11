a5d1dce155a7f051c24623b1dce6152782d28c6b
gopi_plus <gopi_plus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-11 13:36:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/content-text-slider-on-post/trunk@542863 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- help.php
+++
@@ -1,5 +0,0 @@
-<h2>Plugin Management!</h2>
-Click "Go to - Text Management Page" button to add/update/delete the messages. <br />
-Click "Go to - Setting Page" button to update scroller setting.
-<h2>How to use & Faq</h2>
-<a target="_blank" href='http://www.gopiplus.com/work/2012/01/02/content-text-slider-on-post-wordpress-plugin/'>Click here for Live Demo and Faq</a>
